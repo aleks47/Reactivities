ba7446e27b774842835d13e71f807ec9fef95ae8
aleks47 <alecsciocan@gmail.com>
2023-10-29 16:43:38 +0200

    End of section 6

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,17 +1,30 @@
-import { Button, Item, Label, List, Segment } from "semantic-ui-react";
+import { Button, Item, Label, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
+import { SyntheticEvent, useState } from "react";
 
 interface Props {
   activities: Activity[];
   selectActivity: (id: string) => void;
   deleteActivity: (id: string) => void;
+  submitting: boolean;
 }
 
 export default function ActivityList({
   activities,
   selectActivity,
   deleteActivity,
+  submitting,
 }: Props) {
+  const [target, setTarget] = useState("");
+
+  function handleActivityDelete(
+    e: SyntheticEvent<HTMLButtonElement>,
+    id: string
+  ) {
+    setTarget(e.currentTarget.name);
+    deleteActivity(id);
+  }
+
   return (
     <Segment>
       <Item.Group divided>
@@ -28,16 +41,19 @@ export default function ActivityList({
               </Item.Description>
               <Item.Extra>
                 <Button
+                  name={activity.id}
                   floated="right"
                   content="View"
                   color="blue"
                   onClick={() => selectActivity(activity.id)}
                 />
                 <Button
+                  name={activity.id}
+                  loading={submitting && target === activity.id}
                   floated="right"
                   content="Delete"
                   color="red"
-                  onClick={() => deleteActivity(activity.id)}
+                  onClick={(e) => handleActivityDelete(e, activity.id)}
                 />
                 <Label basic content={activity.category} />
               </Item.Extra>
