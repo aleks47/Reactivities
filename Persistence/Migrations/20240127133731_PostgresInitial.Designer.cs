73ece7f1caa21b733573831902302db18d5d7c76
aleks47 <alecsciocan@gmail.com>
2024-01-27 17:09:09 +0200

    Updated buggy URL

--- Persistence/Migrations/20240127133731_PostgresInitial.Designer.cs
+++ Persistence/Migrations/20240127133731_PostgresInitial.Designer.cs
@@ -12,7 +12,7 @@ using Persistence;
 namespace Persistence.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20231210205230_PostgresInitial")]
+    [Migration("20240127133731_PostgresInitial")]
     partial class PostgresInitial
     {
         /// <inheritdoc />
@@ -20,7 +20,7 @@ namespace Persistence.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "7.0.13")
+                .HasAnnotation("ProductVersion", "7.0.11")
                 .HasAnnotation("Relational:MaxIdentifierLength", 63);
 
             NpgsqlModelBuilderExtensions.UseIdentityByDefaultColumns(modelBuilder);
